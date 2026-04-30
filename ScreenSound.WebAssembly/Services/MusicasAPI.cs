8a4b935c6c72ef27135a3b882d8401a893035a89
Marcos Vogado <marcoshcvogado@gmail.com>
2026-04-30 15:21:56 -0300

    feat: music API client

--- ScreenSound.WebAssembly/Services/MusicasAPI.cs
+++ ScreenSound.WebAssembly/Services/MusicasAPI.cs
@@ -5,4 +5,15 @@ namespace ScreenSound.WebAssembly.Services;
 
 public class MusicasAPI
 {
+    private readonly HttpClient _httpClient;
+
+    public MusicasAPI(IHttpClientFactory factory)
+    {
+        _httpClient = factory.CreateClient("API");
+    }
+
+    public async Task<ICollection<MusicaResponse>> GetMusicasAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<ICollection<MusicaResponse>>("musicas");
+    }
 }
