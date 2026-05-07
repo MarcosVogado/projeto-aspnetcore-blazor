f66207c41aaf6bfe270fbbf8f56def9613c82f50
Marcos Vogado <marcoshcvogado@gmail.com>
2026-05-07 15:26:13 -0300

    feat: add logs in playlist page and MusicasAPI

--- ScreenSound.WebAssembly/Services/MusicasAPI.cs
+++ ScreenSound.WebAssembly/Services/MusicasAPI.cs
@@ -6,14 +6,28 @@ namespace ScreenSound.WebAssembly.Services;
 public class MusicasAPI
 {
     private readonly HttpClient _httpClient;
-
-    public MusicasAPI(IHttpClientFactory factory)
+    private readonly ILogger<MusicasAPI> _logger;
+ 
+    public MusicasAPI(IHttpClientFactory factory, ILogger<MusicasAPI> logger)
     {
         _httpClient = factory.CreateClient("API");
+        _logger = logger;
     }
 
     public async Task<ICollection<MusicaResponse>> GetMusicasAsync()
     {
-        return await _httpClient.GetFromJsonAsync<ICollection<MusicaResponse>>("musicas");
+        try
+        {
+            _logger.LogInformation("Iniciando a requisição para obter as músicas.");
+            var resultado = await _httpClient.GetFromJsonAsync<ICollection<MusicaResponse>>("musicas");
+            _logger.LogInformation($"Requisição concluída. Foram obtidas {resultado?.Count ?? 0} músicas.");
+            return resultado;
+        }
+        catch (Exception ex)
+        {
+            string mensagemErro = $"Ocorreu um erro ao obter as músicas.";
+            _logger.LogError(ex, mensagemErro);
+            throw new ApplicationException(mensagemErro, ex);
+        }
     }
 }
